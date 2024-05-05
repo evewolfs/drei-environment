6747691f251ab35c72366be48d4a346e3fbdc5b4
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-05-05 19:47:42 +0200

    stars, cloud, sky, sun position

--- src/Scene.js
+++ src/Scene.js
@@ -1,23 +1,32 @@
-import { OrbitControls, useHelper, Sparkles } from "@react-three/drei";
+import { OrbitControls, useHelper, Sparkles, Stars, Cloud, Sky } from "@react-three/drei";
 import { useRef } from "react";
+import { useControls } from "leva";
 import * as THREE from "three";
 
 const Scene = () => {
   const lightRef = useRef();
   // useHelper(lightRef, THREE.DirectionalLightHelper, 1);
+  const {sunPosition} = useControls({
+    sunPosition: {
+      value: [0,1,0],
+      step: 0.01
+    }
+  })
   return (
     <>
       <OrbitControls />
     
-      <directionalLight castShadow ref={lightRef} position={[1,1,0]} color="purple" intensity={1}/>
+      <directionalLight castShadow ref={lightRef} position={[1,1,0]} color="blue" intensity={1}/>
 
       {/* <mesh castShadow>
         <boxGeometry />
         <meshStandardMaterial color="#C7CAC7" />
       </mesh> */}
 
-      <Sparkles count={100} speed={0.5} opacity={3} color="red" scale={[10,10,10]} size={2}/>
-
+      {/* <Sparkles count={100} speed={0.5} opacity={3} color="red" scale={[10,10,10]} size={2}/> */}
+      <Stars radius={2} depth={50} count={5000} factor={4} saturation={0} fade speed={1} segments={4} depthTest={false} />
+      <Cloud opacity={1} speed={0.2} depth={4}/>
+      <Sky sunPosition={sunPosition} />
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry args={[8, 8]} />
         <meshStandardMaterial color="#CC3941" />

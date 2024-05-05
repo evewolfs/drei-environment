a508b5451080b058b72b6b28dca6a750dee4049e
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-05-05 19:31:49 +0200

    Sparkles helper

--- src/Scene.js
+++ src/Scene.js
@@ -1,20 +1,22 @@
-import { OrbitControls, useHelper } from "@react-three/drei";
+import { OrbitControls, useHelper, Sparkles } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 
 const Scene = () => {
   const lightRef = useRef();
-  useHelper(lightRef, THREE.DirectionalLightHelper, 1);
+  // useHelper(lightRef, THREE.DirectionalLightHelper, 1);
   return (
     <>
       <OrbitControls />
     
       <directionalLight castShadow ref={lightRef} position={[1,1,0]} color="purple" intensity={1}/>
 
-      <mesh castShadow>
+      {/* <mesh castShadow>
         <boxGeometry />
         <meshStandardMaterial color="#C7CAC7" />
-      </mesh>
+      </mesh> */}
+
+      <Sparkles count={100} speed={0.5} opacity={3} color="red" scale={[10,10,10]} size={2}/>
 
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry args={[8, 8]} />

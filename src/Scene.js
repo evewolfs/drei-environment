f64bfda57e3920dc90b3821819704b9ddac1d87a
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-05-05 19:53:24 +0200

    environment map

--- src/Scene.js
+++ src/Scene.js
@@ -1,4 +1,4 @@
-import { OrbitControls, useHelper, Sparkles, Stars, Cloud, Sky } from "@react-three/drei";
+import { OrbitControls, useHelper, Sparkles, Stars, Cloud, Sky, Environment } from "@react-three/drei";
 import { useRef } from "react";
 import { useControls } from "leva";
 import * as THREE from "three";
@@ -26,7 +26,16 @@ const Scene = () => {
       {/* <Sparkles count={100} speed={0.5} opacity={3} color="red" scale={[10,10,10]} size={2}/> */}
       <Stars radius={2} depth={50} count={5000} factor={4} saturation={0} fade speed={1} segments={4} depthTest={false} />
       <Cloud opacity={1} speed={0.2} depth={4}/>
-      <Sky sunPosition={sunPosition} />
+      {/* <Sky sunPosition={sunPosition} /> */}
+
+      <Environment background files={[
+        "./envMap/px.png",
+        "./envMap/nx.png",
+        "./envMap/py.png",
+        "./envMap/ny.png",
+        "./envMap/pz.png",
+        "./envMap/nz.png",
+      ]} />
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry args={[8, 8]} />
         <meshStandardMaterial color="#CC3941" />
